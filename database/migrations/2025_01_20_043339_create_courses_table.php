6a2d483ee2bab96d06288330eedd16fdc5375c7a
ifalfahri <ifaltampan69@gmail.com>
2025-01-20 12:43:25 +0800

    feat: add fields to Course migration for name, description, price, status, and students count

--- database/migrations/2025_01_20_043339_create_courses_table.php
+++ database/migrations/2025_01_20_043339_create_courses_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->boolean('status')->default(true);
+            $table->integer('students_count')->default(0);
             $table->timestamps();
         });
     }
